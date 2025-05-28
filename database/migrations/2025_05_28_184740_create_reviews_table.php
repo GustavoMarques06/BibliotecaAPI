aa6a230fe88f1974b42b5ebbb03c09c5cfe0afae
Gustavo Marques Martins <gustavomqm@hotmail.com>
2025-05-28 17:03:29 -0300

    MODELS/MIGRATIONS

---
+++ database/migrations/2025_05_28_184740_create_reviews_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('reviews', function (Blueprint $table) {
+            $table->id();
+            $table->integer('nota');
+            $table->string('texto')->nullable();
+            $table->foreignId('usuarios_id')->constrained('usuarios')->onDelete('cascade');
+            $table->foreignId('livros_id')->constrained('livros')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('reviews');
+    }
+};
